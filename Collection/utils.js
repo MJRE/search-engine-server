76cc82a0fcf09b885582e0edf7276cfaf45fdcb9
unknown <mjr115588@gmail.com>
2020-10-17 10:35:47 -0700

    Fix (0 < similarity < 1) problem v3

--- Collection/utils.js
+++ Collection/utils.js
@@ -121,8 +121,7 @@ function getSearchResult(similarityMap, documents){
         if(similarityMap.get(object.key) > 0.0 && object.key != 'Query'){
             obj.key = object.key
             obj.content = object.content
-            sim = similarityMap.get(object.key);
-            obj.similarity = (sim > 0.0 && sim <= 1)? 1:parseInt((sim * 100))
+            obj.similarity = parseInt((similarityMap.get(object.key) * 100))
             result.push(obj)
         }
     });
